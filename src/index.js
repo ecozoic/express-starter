a528359cb20b28e785647c0356af79bdbe33a7f9
ecozoic <johnnn.mitchell@gmail.com>
2017-08-28 18:45:12 -0400

    add deps and middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
+
+const PORT = process.env.PORT || 8080;
+const NODE_ENV = process.env.NODE_ENV || 'production';
+
+app.use(compression());
+app.use(bodyParser.json());
+
+if (NODE_ENV === 'development') {
+  app.use(morgan());
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`App listening on port: ${PORT} `)
