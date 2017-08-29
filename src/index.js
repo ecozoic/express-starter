f49242d85a44ff96e350957c9bd776a8dafb5d49
ecozoic <johnnn.mitchell@gmail.com>
2017-08-28 21:16:34 -0400

    handle 404s and 500s

--- src/index.js
+++ src/index.js
@@ -22,8 +22,20 @@ app.get('/', (req, res) => {
   res.json({ id: 1, message: 'Hello world!' });
 });
 
+app.get('/error', (req, res) => {
+  throw Error('ABORT');
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('We ain\'t found shit');
+});
+
 if (NODE_ENV === 'development') {
   app.use(errorHandler());
+} else {
+  app.use((err, req, res, next) => {
+    res.status(500).send('Shit\'s broke yo');
+  });
 }
 
 app.listen(PORT, () => {
