873452c1eeee8fb2361383b589f846c413dfd885
ecozoic <johnnn.mitchell@gmail.com>
2017-08-28 20:56:46 -0400

    env configuration

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
+require('dotenv').config();
+
 const express = require('express');
 const compression = require('compression');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const errorHandler = require('errorhandler');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-const NODE_ENV = process.env.NODE_ENV || 'production';
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.use(compression());
 app.use(bodyParser.json());
 
 if (NODE_ENV === 'development') {
-  app.use(morgan());
+  app.use(morgan('tiny'));
 }
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({ id: 1, message: 'Hello world!' });
 });
+
+if (NODE_ENV === 'development') {
+  app.use(errorHandler());
+}
 
 app.listen(PORT, () => {
   console.log(`App listening on port: ${PORT} `)
